6cd073ed0f9e2b5e374404aadadd10c9e8f6ad90
Matishac14 <matias.fernandez.zamorano@gmail.com>
2026-07-06 18:25:27 -0400

    FIX: correción busqueda de ID en Bucket S3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_public_access_block" "this" {
 }
 
 resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.this
+  bucket = aws_s3_bucket.this.id
 
   versioning_configuration {
     status = "Enabled"
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_versioning" "this" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  bucket = aws_s3_bucket.this
+  bucket = aws_s3_bucket.this.id
 
   rule {
     apply_server_side_encryption_by_default {
